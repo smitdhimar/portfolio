b70e60d79b6ff2cedd69b2e0c8299f71e42b105c
smit dhimar <smitdhimar61@gmail.com>
2025-11-24 22:14:26 -0500

    feat: add fallback passing to config.tsx

--- config.tsx
+++ config.tsx
@@ -1,10 +1,10 @@
 const userDetails ={
-    name: process.env.NEXT_PUBLIC_NAME, 
-    siteTitle: process.env.NEXT_PUBLIC_SITE_TITLE || "Portfolio"
+    name: process.env.NEXT_PUBLIC_NAME || "Smit Dhimar", 
+    siteTitle: process.env.NEXT_PUBLIC_SITE_TITLE || "Portfolio | Smit Dhimar"
 } 
 
 const contactDetails = {
-    emailId : process.env.NEXT_PUBLIC_EMAIL_ID,
-    socialMediaUsername : process.env.NEXT_PUBLIC_SOCIAL_MEDIA_USERNAME
+    emailId : process.env.NEXT_PUBLIC_EMAIL_ID || "smitdhimar61@gmail.com",
+    socialMediaUsername : process.env.NEXT_PUBLIC_SOCIAL_MEDIA_USERNAME || "smitdhimar"
 }
 export { userDetails, contactDetails}
